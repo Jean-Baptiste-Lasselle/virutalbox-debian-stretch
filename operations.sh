5e7393d4e40fe65d826d5a07032a2a6129c49bf2
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-03-02 19:45:14 +0100

    Update operations.sh

--- operations.sh
+++ operations.sh
@@ -7,7 +7,7 @@ rm -f ./voyons.ca
 
 export NOM_DE_CODE_RELEASE_DEBIAN=$(lsb_release -a|grep Codename|awk -F ']' '{print $1}'|awk '{print $2}')
 
-wget -O nom_fichier_package_debian_a_telecharger https://download.virtualbox.org/virtualbox/6.0.4/  
+wget -O nom_fichier_package_debian_a_telecharger https://download.virtualbox.org/virtualbox/$DERNIERE_VERSION_VIRTUALBOX_DISTRIBUEE/  
 cat ./nom_fichier_package_debian_a_telecharger |grep amd64|grep Debian|grep $NOM_DE_CODE_RELEASE_DEBIAN|awk -F '"' '{print $2}'
 # exemple : virtualbox-6.0_6.0.4-128413~Debian~stretch_amd64.deb
 export NOM_FICHIER_PACKAGE_DEBIAN=$(cat ./nom_fichier_package_debian_a_telecharger |grep amd64|grep Debian|grep stretch|awk -F '"' 
@@ -15,10 +15,9 @@ export NOM_FICHIER_PACKAGE_DEBIAN=$(cat ./nom_fichier_package_debian_a_telecharg
 # télécharger le fichier *.deb d'installation de virtualbox sous debian stretch
 # J'ai trouvé ces 3 URI de téléchargement, en allant sur le site officiel de virutalbox, au menu "dowloads", i.e. 
 # https://www.virtualbox.org/wiki/Linux_Downloads
-export URI_TELECHARGEMENT_PACKAGE_VBOX_DEBIAN=https://download.virtualbox.org/virtualbox/$DERNIERE_VERSION_VIRTUALBOX_DISTRIBUEE/virtualbox-6.0_6.0.4-128413~Debian~stretch_amd64.deb
-export NOM_FICHIER_DEB_INSTALLATION_VBOX=virtualbox-6.0_6.0.4-128413~Debian~stretch_amd64.deb
-export CHECKSUM_MD5=https://www.virtualbox.org/download/hashes/6.0.4/MD5SUMS
-export CHECKSUM_SHA2=https://www.virtualbox.org/download/hashes/6.0.4/SHA256SUMS
+export URI_TELECHARGEMENT_PACKAGE_VBOX_DEBIAN=https://download.virtualbox.org/virtualbox/$DERNIERE_VERSION_VIRTUALBOX_DISTRIBUEE/$NOM_FICHIER_PACKAGE_DEBIAN
+export CHECKSUM_MD5=https://www.virtualbox.org/download/hashes/$DERNIERE_VERSION_VIRTUALBOX_DISTRIBUEE/MD5SUMS
+export CHECKSUM_SHA2=https://www.virtualbox.org/download/hashes/$$DERNIERE_VERSION_VIRTUALBOX_DISTRIBUEE/SHA256SUMS
 
 
 
@@ -52,7 +51,7 @@ echo "Et bingo, nous sommes bons! "
 echo "Notons : l'idéal sera, pour une recette de \"production\", d'installer ces dépendances avant même l'exécuation [dpkg -i \$NOM_FICHIER_PACKAGE_LINUX_DEBIAN]"
 
 
-# Nous veneons d'installer un package debian (virutalbox), manuellement. Il peut lui manquer des dépendances.
+# Nous venons d'installer un package debian (virutalbox), manuellement. Il peut lui manquer des dépendances.
 # apt-get a la capacité de résoudre ces dépendances automatiquement, avec la commande : 
 # (sychronisation package manager)
 sudo apt-get -f install
